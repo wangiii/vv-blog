90b8b74b1de73452c7d0bd7213a0cda12dc9de25
JasonVV <jasonwangiii@outlook.com>
2019-09-09 15:41:41 +0800

    Feat: add overtrue/laravel-lang

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');

25db1139aa1c9429d36a200fa42c4026f3b7c696
JasonVV <jasonwangiii@outlook.com>
2019-09-10 16:05:57 +0800

    Feat: 后台

---
+++ app/Admin/Controllers/ArticleController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Article;
+use App\Models\Category;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+use http\Env\Request;
+
+class ArticleController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'App\Models\Article';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Article);
+        $grid->column('title', '标题');
+        $grid->column('describe', '简介');
+//        $grid->column('content', '内容');
+        $grid->column('created_at', '创建时间');
+        $grid->column('updated_at', '更新时间');
+        $grid->column('category_id', '分类')->display(function () {
+            return $this->category->name;
+        });
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Article::findOrFail($id));
+        $show->getModel()->load('category');
+
+        $show->field('id', 'ID');
+        $show->field('title', '标题');
+        $show->field('describe', '简介');
+        $show->field('content', '正文');
+        $show->field('created_at', '创建时间');
+        $show->field('updated_at', '更新时间');
+        $show->field('category_id', '分类')->as(function () {
+            return $this->category->name;
+        });
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Article);
+
+        $form->display('id', 'ID');
+        $form->text('title', '标题');
+        $form->text('describe', '简介');
+        $form->textarea('content', '内容');
+        $form->select('category_id', '分类')
+            ->options(Category::all()->pluck('name', 'id'))
+            ->rules('required');
+
+        $form->footer(function ($footer) {
+            $footer->disableEditingCheck();
+            $footer->disableCreatingCheck();
+            $footer->disableViewCheck();
+        });
+
+        return $form;
+    }
+
+    public function index(Content $content)
+    {
+        return $content
+            ->header('文章列表')
+            ->body($this->grid());
+    }
+
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('文章')
+            ->body($this->form()->edit($id));
+    }
+
+    public function create(Content $content)
+    {
+        return $content
+            ->header('创建文章')
+            ->body($this->form());
+    }
+
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header("文章详情")
+            ->body($this->detail($id));
+    }
+}
---
+++ app/Admin/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\Dashboard;
+use Encore\Admin\Layout\Column;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\Row;
+
+class HomeController extends Controller
+{
+    public function index(Content $content)
+    {
+        return $content
+            ->title('仪表盘')
+            ->row(Dashboard::title())
+            ->row(function (Row $row) {
+
+                $row->column(3, function (Column $column) {
+                    $column->append(Dashboard::environment());
+                });
+
+                $row->column(3, function (Column $column) {
+                    $column->append(Dashboard::dependencies());
+                });
+            });
+    }
+}

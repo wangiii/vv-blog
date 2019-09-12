1f758ed0765c7182d0e4c46416e6e346fe9a8996
JasonVV <jasonwangiii@outlook.com>
2019-09-13 01:34:08 +0800

    Update : 加入 GitHub 链接

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="blog-footer">
-    <p>&copy; 2019 <span style="color: #e27575;font-size: 14px;">❤</span> JasonVV 版权所有</p>
+    <p>&copy; 2019 <span style="color: #e27575;font-size: 14px;">❤</span> <a href="https://github.com/wangiii" class="text-decoration-none">JasonVV</a> 版权所有</p>
 </footer>

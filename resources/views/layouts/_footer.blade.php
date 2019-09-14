53f939850c741f3bd73cc52398e8073702a3fe01
JasonVV <jasonwangiii@outlook.com>
2019-09-14 11:16:32 +0800

    fix footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="blog-footer">
-    <p>&copy; {{ Carbon\Carbon::now()->year }} <span style="color: #e27575;font-size: 14px;">❤</span> <a href="https://github.com/wangiii" class="text-decoration-none" style="color: lightgray">JasonVV</a> 版权所有</p>
+    <p>&copy; 2018-{{ Carbon\Carbon::now()->year }} <span style="color: #e27575;font-size: 14px;">❤</span> <a href="https://github.com/wangiii" class="text-decoration-none" style="color: lightgray">JasonVV</a> 版权所有</p>
 </footer>

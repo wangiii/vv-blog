ed40dbf5ed705b90541ab48d1d281ec78e5589e3
JasonVV <jasonwangiii@outlook.com>
2019-09-22 00:29:29 +0800

    update: 更新 footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="blog-footer">
     <p>
         <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1278024679'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s9.cnzz.com/z_stat.php%3Fid%3D1278024679%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
-        &copy; 2018-{{ Carbon\Carbon::now()->year }} <span style="color: #e27575;font-size: 14px;">❤</span> <a href="https://github.com/wangiii" class="text-decoration-none" style="color: lightgray">JasonVV</a> 版权所有
+        &copy; 2018-{{ Carbon\Carbon::now()->year }} 由 <a href="https://github.com/wangiii" class="text-decoration-none" style="color: lightgray">JasonVV</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
 </footer>

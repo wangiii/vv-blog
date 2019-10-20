98b55be601ac9f60cd78ac8506335c9d34672f57
JasonVV <jasonwangiii@outlook.com>
2019-10-21 02:23:12 +0800

    update

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="blog-footer">
     <p>
         <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1278024679'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s9.cnzz.com/z_stat.php%3Fid%3D1278024679%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
-        &copy; 2018-{{ Carbon\Carbon::now()->year }} JasonVV, unless otherwise noted.
+        &copy; {{ Carbon\Carbon::now()->year }} JasonVV, unless otherwise noted.
     </p>
 </footer>

179e15a3a1e2f46a8ce8712fb9eb9c4a7bb78a8b
JasonVV <jasonwangiii@outlook.com>
2019-10-21 01:20:16 +0800

    fix

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -27,7 +27,7 @@ class ArticlesController extends Controller
             ->where('category_id', '!=', 3) // 过滤算法题
             ->with('tags', 'category')
             ->orderBy('created_at', 'desc')
-            ->paginate(10);
+            ->paginate(8);
 
         $articles = new Collection($articlesPaginator->items(), $this->articleTransformer);
         $articles->setPaginator(new IlluminatePaginatorAdapter($articlesPaginator));
